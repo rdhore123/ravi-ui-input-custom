42f03b20a79675945689cca53d50f3417ba78377
Ravindra Dhore <r.dhore@easternenterprise.com>
2023-02-22 14:15:32 +0530

    First package commit

---
+++ heading.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <template v-if="variant == 'h1'">
+      <h1 class="oldclass h1" v-bind="$attrs">
+        <slot />
+      </h1>
+    </template>
+    <template v-if="variant == 'h2'">
+      <h2 class="oldclass h2" v-bind="$attrs">
+        <slot />
+      </h2>
+    </template>
+  </div>
+</template>
+<script>
+export default {
+  inheritAttrs: false,
+  props: {
+    variant: {
+      type: String,
+      required: false,
+    },
+  },
+};
+</script>
